7e55ff46a41d1cfadb0f35939579b9406b4052d6
Bohár Dávid <89093467+F4llingf0x@users.noreply.github.com>
2026-06-30 19:45:31 +0200

    init commit

---
+++ src/main/java/com/fallingfox/kahootgenerator/KahootGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.fallingfox.kahootgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KahootGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KahootGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fallingfox/kahootgenerator/KahootGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fallingfox.kahootgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KahootGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
